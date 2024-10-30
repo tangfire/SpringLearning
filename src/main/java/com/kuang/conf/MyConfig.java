e98900d69911f4518cc7282fa85ea8ed40151ce8
tangfire <52628216@qq.com>
2024-10-30 12:33:25 +0800

    静态代理

---
+++ src/main/java/com/kuang/conf/MyConfig.java
@@ -0,0 +1,15 @@
+package com.kuang.conf;
+
+import com.kuang.pojo05.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  //代表这是一个配置类
+public class MyConfig {
+
+   @Bean //通过方法注册一个bean，这里的返回值就Bean的类型，方法名就是bean的id！
+   public Dog dog(){
+       return new Dog();
+  }
+
+}
--- src/test/java/MyTest04.java
+++ src/test/java/MyTest04.java
@@ -1,6 +1,9 @@
+import com.kuang.conf.MyConfig;
 import com.kuang.pojo04.User;
+import com.kuang.pojo05.Dog;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest04 {
@@ -11,5 +14,14 @@ public class MyTest04 {
         User user = (User) applicationContext.getBean("user");
         System.out.println(user.name);
     }
+
+    @Test
+    public void test18(){
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Dog dog = (Dog) applicationContext.getBean("dog");
+        System.out.println(dog.name);
+    }
+
 
 }
